8a44d6e2b6ae788850328e9fe704f0e06f44282a
WaroonRagwongsiri <waroonwork@gmail.com>
2025-07-30 09:22:37 +0700

    debounce

--- Activity5.5/Activity5.5.ino
+++ Activity5.5/Activity5.5.ino
@@ -20,6 +20,14 @@ const int	segment_pin[] = {5,6,7,8,9,10,11,12};
 static int	g_ans = 1;
 static int	g_guess = 0;
 
+// debounce
+static int		sw_state[2];
+static int		last_sw_state[2] = {HIGH, HIGH};
+unsigned long	last_debounce_time[2] = {0, 0};
+unsigned long	debounce_delay = 10;
+static int		reading[2] = {0, 0};
+static int		timeread = millis();
+
 void	setup()
 {
 	pinMode(SW_A, INPUT_PULLUP);
@@ -66,38 +74,62 @@ void	set_zero()
 
 void	handle_add_button()
 {
-	int reading = digitalRead(SW_A);
-	if (reading == LOW)
+	reading[0] = digitalRead(SW_A);
+	if (reading[0] != last_sw_state[0])
 	{
-		g_ans++;
-		delay(250);
-		if (g_ans > 9)
+		last_debounce_time[0] = millis();
+	}
+	if (millis() - last_debounce_time[0] > debounce_delay)
+	{
+		if (reading[0] != sw_state[0])
 		{
-			g_ans = 1;
+			sw_state[0] = reading[0];
+
+			if (sw_state[0] == LOW)
+			{
+				g_ans++;
+				if (g_ans > 9)
+				{
+					g_ans = 1;
+				}
+			}
 		}
 	}
+	last_sw_state[0] = reading[0];
 }
 
 void	handle_guess_button()
 {
-	int reading = digitalRead(SW_G);
-	if (reading == LOW)
+	reading[1] = digitalRead(SW_G);
+	if (reading[1] != last_sw_state[1])
 	{
-		delay(250);
-		if (g_ans == g_guess)
+		last_debounce_time[1] = millis();
+	}
+	if (millis() - last_debounce_time[1] > debounce_delay)
+	{
+		if (reading[1] != sw_state[1])
 		{
-			g_ans = 0;
-			g_guess = random(1, 10);
-			Serial.println(g_guess);
-			return ;
-		}
-		else if (g_ans > g_guess)
-		{
-			g_ans = 10;
-		}
-		else if (g_ans < g_guess)
-		{
-			g_ans = 11;
+			sw_state[1] = reading[1];
+
+			if (sw_state[1] == LOW)
+			{
+				if (g_ans == g_guess)
+				{
+					g_ans = 0;
+					g_guess = random(1, 10);
+					Serial.println(g_guess);
+					return ;
+				}
+				else if (g_ans > g_guess)
+				{
+					g_ans = 10;
+				}
+				else if (g_ans < g_guess)
+				{
+					g_ans = 11;
+				}
+			}
 		}
 	}
+	last_sw_state[1] = reading[1];
 }
